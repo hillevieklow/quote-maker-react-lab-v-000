ddf73e7c53652a45f66275e083e24ff99e7b255e
Hillevi Eklöw <hillevieklow@gmail.com>
2019-03-19 23:51:34 +0000

    Automatically backed up by Learn

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,27 +1,35 @@
-export const addQuote = quote => {
-    return {
-      type: 'ADD_QUOTE',
-      quote: Object.assign({}, quote, { votes: 0 })
-    }
-  }
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
-  export const removeQuote = quoteId => {
-    return {
-      type: 'REMOVE_QUOTE',
-      quoteId
-    }
-  }
+class Quotes extends Component {
 
-  export const upvoteQuote = quoteId => {
-    return {
-      type: 'UPVOTE_QUOTE',
-      quoteId
-    }
+  render() {
+    const { quotes, removeQuote, upvoteQuote, downvoteQuote } = this.props;
+    return (
+      <div>
+        <hr />
+        <div className="row justify-content-center">
+          <h2>Quotes</h2>
+        </div>
+        <hr />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              {quotes.map(quote => <QuoteCard key={quote.id} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote} removeQuote={removeQuote} quote={quote} />)}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
+}
 
-  export const downvoteQuote = quoteId => {
-    return {
-      type: 'DOWNVOTE_QUOTE',
-      quoteId
-    }
-  }
+const mapStateToProps = state => {
+  return ({
+    quotes: state.quotes
+  })
+}
+
+export default connect(mapStateToProps, { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
